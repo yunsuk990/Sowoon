81015cf296aa1d1732ded4b2caf94e84058d5e0c
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-01-06 19:31:52 +0900

    Create ArtistProfileFragment.kt

---
+++ app/src/main/java/com/example/sowoon/ArtistProfileFragment.kt
@@ -0,0 +1,37 @@
+package com.example.sowoon
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.sowoon.data.entity.Profile
+import com.example.sowoon.databinding.FragmentArtistProfileBinding
+import com.google.gson.Gson
+
+class ArtistProfileFragment : Fragment() {
+
+    lateinit var binding: FragmentArtistProfileBinding
+    private var gson = Gson()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentArtistProfileBinding.inflate(inflater, container, false)
+
+        val profileJson = arguments?.getString("profile")
+        val profile = gson.fromJson(profileJson, Profile::class.java)
+        setProfile(profile)
+
+        return binding.root
+    }
+
+    private fun setProfile(profile: Profile){
+        binding.artistsProfileName.text = profile.name
+        binding.artistsProfileSchoolInput.text = profile.school
+        binding.artistsProfileAwardsInput.text = profile.awards
+    }
+
+}
