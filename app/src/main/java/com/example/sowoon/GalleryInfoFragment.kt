3ce5b3a0d2678f432931b6336a5529be39e8ea73
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-01-06 19:31:28 +0900

    Update GalleryInfoFragment.kt

--- app/src/main/java/com/example/sowoon/GalleryInfoFragment.kt
+++ app/src/main/java/com/example/sowoon/GalleryInfoFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.sowoon.data.entity.Gallery
 import com.example.sowoon.databinding.FragmentGalleryInfoBinding
 import com.google.gson.Gson
 
