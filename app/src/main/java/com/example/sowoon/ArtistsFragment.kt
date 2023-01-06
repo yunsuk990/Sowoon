fecd080c5e6dc493d30cb2cbe74b7aba21fd1ecd
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-01-06 19:31:57 +0900

    Update ArtistsFragment.kt

--- app/src/main/java/com/example/sowoon/ArtistsFragment.kt
+++ app/src/main/java/com/example/sowoon/ArtistsFragment.kt
@@ -7,8 +7,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.sowoon.data.entity.Profile
 import com.example.sowoon.databinding.FragmentArtistsBinding
-import com.example.sowoon.databinding.FragmentMainBinding
+import com.google.gson.Gson
 
 class ArtistsFragment : Fragment() {
 
@@ -22,11 +23,28 @@ class ArtistsFragment : Fragment() {
 
 
         val adapter = ArtistsProfileRV()
-        val profile1 = Profile("정은숙")
+        val profile1 = Profile("정은숙", "홍익대학교", "한국미술대회 1위")
         adapter.addProfile(profile1)
         binding.artistsRv.adapter = adapter
         binding.artistsRv.layoutManager = LinearLayoutManager(context, GridLayoutManager.HORIZONTAL, false)
 
+        adapter.itemClickListener(object: ArtistsProfileRV.MyItemClickOnListener{
+            override fun profileClick(profile: Profile) {
+                profileArtist(profile)
+            }
+        })
         return binding.root
+    }
+
+    private fun profileArtist(profile: Profile){
+        (context as MainActivity).supportFragmentManager.beginTransaction()
+            .replace(R.id.main_frame, ArtistProfileFragment().apply {
+                arguments = Bundle().apply {
+                    val gson = Gson()
+                    val profileJson = gson.toJson(profile)
+                    putString("profile", profileJson)
+                }
+            })
+            .commitNowAllowingStateLoss()
     }
 }
