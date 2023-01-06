84205665e21a02dd8da3c4dd85b81757a2f72a42
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-01-06 19:31:54 +0900

    Update ArtistsProfileRV.kt

--- app/src/main/java/com/example/sowoon/ArtistsProfileRV.kt
+++ app/src/main/java/com/example/sowoon/ArtistsProfileRV.kt
@@ -3,11 +3,22 @@ package com.example.sowoon
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.sowoon.data.entity.Profile
 import com.example.sowoon.databinding.ItemArtistsprofileBinding
 
 class ArtistsProfileRV: RecyclerView.Adapter<ArtistsProfileRV.ViewHolder>() {
 
     private val profileList = ArrayList<Profile>()
+
+    interface MyItemClickOnListener {
+        fun profileClick(profile: Profile)
+    }
+
+    private lateinit var mItemClickListener: MyItemClickOnListener
+
+    fun itemClickListener(itemClick: MyItemClickOnListener){
+        mItemClickListener = itemClick
+    }
 
     inner class ViewHolder(val binding: ItemArtistsprofileBinding): RecyclerView.ViewHolder(binding.root){
         fun bind(profile: Profile){
@@ -22,7 +33,9 @@ class ArtistsProfileRV: RecyclerView.Adapter<ArtistsProfileRV.ViewHolder>() {
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind(profileList[position])
-
+        holder.binding.artistsProfile.setOnClickListener{
+            mItemClickListener.profileClick(profileList[position])
+        }
     }
 
     override fun getItemCount(): Int = profileList.size
