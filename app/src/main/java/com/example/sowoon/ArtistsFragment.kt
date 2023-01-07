34f6817da463aa56adbec161476e54361b0dcde7
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-01-07 23:01:31 +0900

    Update ArtistsFragment.kt

--- app/src/main/java/com/example/sowoon/ArtistsFragment.kt
+++ app/src/main/java/com/example/sowoon/ArtistsFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.sowoon.data.entity.Gallery
 import com.example.sowoon.data.entity.Profile
 import com.example.sowoon.databinding.FragmentArtistsBinding
 import com.google.gson.Gson
@@ -21,9 +22,11 @@ class ArtistsFragment : Fragment() {
     ): View? {
         binding = FragmentArtistsBinding.inflate(inflater, container, false)
 
+        //example gallery
+        var exampleGallery = Gallery("소운", "정은숙", "2020년 작품", R.drawable.galleryexp3)
 
         val adapter = ArtistsProfileRV()
-        val profile1 = Profile("정은숙", 58,"홍익대학교", "한국미술대회 1위")
+        val profile1 = Profile("정은숙", 58,"홍익대학교", "한국미술대회 1위",null, exampleGallery)
         adapter.addProfile(profile1)
         binding.artistsRv.adapter = adapter
         binding.artistsRv.layoutManager = LinearLayoutManager(context, GridLayoutManager.VERTICAL, false)
@@ -31,6 +34,10 @@ class ArtistsFragment : Fragment() {
         adapter.itemClickListener(object: ArtistsProfileRV.MyItemClickOnListener{
             override fun profileClick(profile: Profile) {
                 profileArtist(profile)
+            }
+
+            override fun profileArtworkClick(profile: Profile) {
+                profileArtwork(profile)
             }
         })
         return binding.root
@@ -43,6 +50,19 @@ class ArtistsFragment : Fragment() {
                     val gson = Gson()
                     val profileJson = gson.toJson(profile)
                     putString("profile", profileJson)
+                }
+            })
+            .commitNowAllowingStateLoss()
+    }
+
+    private fun profileArtwork(profile: Profile){
+        (context as MainActivity).supportFragmentManager.beginTransaction()
+            .replace(R.id.main_frame, GalleryInfoFragment().apply {
+                arguments = Bundle().apply {
+                    val gallery = profile.bestArtwork
+                    val gson = Gson()
+                    val galleryJson = gson.toJson(gallery)
+                    putString("gallery", galleryJson)
                 }
             })
             .commitNowAllowingStateLoss()
