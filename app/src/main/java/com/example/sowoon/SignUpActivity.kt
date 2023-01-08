198a3355750ab8bf2ceab9eb0c419489c25c4594
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-01-08 15:11:14 +0900

    Create SignUpActivity

---
+++ app/src/main/java/com/example/sowoon/SignUpActivity.kt
@@ -0,0 +1,15 @@
+package com.example.sowoon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sowoon.databinding.ActivitySignUpBinding
+
+class SignUpActivity : AppCompatActivity() {
+    lateinit var binding: ActivitySignUpBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
