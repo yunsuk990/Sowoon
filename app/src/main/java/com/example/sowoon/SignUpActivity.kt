64854f9e8b092da883930f68b048a495c4eb3df9
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-01-09 23:49:04 +0900

    Create SignUpActivity

--- app/src/main/java/com/example/sowoon/SignUpActivity.kt
+++ app/src/main/java/com/example/sowoon/SignUpActivity.kt
@@ -2,14 +2,24 @@ package com.example.sowoon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sowoon.data.entity.User
 import com.example.sowoon.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
     lateinit var binding: ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+
+
+    private fun signUp(){
+
+        var expUser = User(1, "yunsuk990@naver.com", "최윤석",25, "yunsuk123")
+        //DB중복 아이디 확인.. 없으면 회원가입
+
+
     }
 }
