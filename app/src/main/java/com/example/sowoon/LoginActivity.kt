7f7cef9bba78977dc431183300f2fb2aec2b7d7b
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-01-09 23:49:13 +0900

    Create LoginActivity

--- app/src/main/java/com/example/sowoon/LoginActivity.kt
+++ app/src/main/java/com/example/sowoon/LoginActivity.kt
@@ -1,7 +1,11 @@
 package com.example.sowoon
 
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.sowoon.data.entity.User
 import com.example.sowoon.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
@@ -12,5 +16,43 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.loginBtn.setOnClickListener {
+            login()
+        }
+    }
+
+    private fun login(){
+        if(binding.loginIdEt.toString().isEmpty()){
+            Toast.makeText(this, "이메일을 입력해주세요.", Toast.LENGTH_SHORT).show()
+            return
+        }
+        if(binding.loginPasswordEt.toString().isEmpty()){
+            Toast.makeText(this, "비밀번호를 입력해주세요.", Toast.LENGTH_SHORT).show()
+            return
+        }
+        var email = binding.loginIdEt.toString()
+        var password = binding.loginPasswordEt.toString()
+
+        //DB 조회..
+        var expUser = User(1, "yunsuk990@naver.com", "최윤석",25, "yunsuk123")
+
+        if(expUser != null){
+            saveJwt(expUser.id)
+            startMainActivity()
+        }else{
+            Toast.makeText(this, "회원정보가 존재하지 않습니다.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun startMainActivity() {
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+
+    private fun saveJwt(jwt: Int) {
+        val spf = getSharedPreferences("auth", MODE_PRIVATE)
+        val editor = spf.edit()
+        editor.putInt("jwt", jwt)
+        editor.apply()
     }
 }
