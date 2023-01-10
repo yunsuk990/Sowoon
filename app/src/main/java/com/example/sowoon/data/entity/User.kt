d1fdf8f71995a6eca75039bd70f075d7834f650f
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-01-10 23:59:18 +0900

    Update User.kt

--- app/src/main/java/com/example/sowoon/data/entity/User.kt
+++ app/src/main/java/com/example/sowoon/data/entity/User.kt
@@ -1,9 +1,13 @@
 package com.example.sowoon.data.entity
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "UserTable")
 data class User(
-    var id: Int,
     var email: String,
     var name: String,
-    var age: Int,
-    var password: String
+    var age: String,
+    var password: String,
+    @PrimaryKey(autoGenerate = true) var id: Int = 0
 )
