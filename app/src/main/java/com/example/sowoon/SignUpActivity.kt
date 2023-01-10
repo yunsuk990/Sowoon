e379a22fd95404e7f5f98d241d71c0080771bb86
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-01-10 23:59:47 +0900

    Update SignUpActivity.kt

--- app/src/main/java/com/example/sowoon/SignUpActivity.kt
+++ app/src/main/java/com/example/sowoon/SignUpActivity.kt
@@ -2,11 +2,17 @@ package com.example.sowoon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.sowoon.data.entity.User
+import com.example.sowoon.database.Database
 import com.example.sowoon.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+
     lateinit var binding: ActivitySignUpBinding
+    lateinit var database: Database
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
@@ -16,8 +22,19 @@ class SignUpActivity : AppCompatActivity() {
 
 
     private fun signUp(){
+        database = Database.getInstance(applicationContext)!!
+        var email = binding.idEt.text.toString()
+        var password = binding.passwordEt.text.toString()
+        var passwordCheck = binding.passwordcheckEt.text.toString()
+        var age = binding.ageEt.text.toString()
 
-        var expUser = User(1, "yunsuk990@naver.com", "최윤석",25, "yunsuk123")
+        if(database.userDao().getUser(email, password) == null){
+            database.userDao().insertUser(User(email, binding.nameEt.text.toString(), age, password))
+        }else{
+            Toast.makeText(applicationContext, "이미 가입되어 있는 계정입니다.", Toast.LENGTH_SHORT).show()
+        }
+
+        //var expUser = User("yunsuk990@naver.com", "최윤석","25", "yunsuk123")
         //DB중복 아이디 확인.. 없으면 회원가입
 
 
