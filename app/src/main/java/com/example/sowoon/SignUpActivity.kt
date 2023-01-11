fb0a77778167d53963de22cc8acbcbcbbcb6d2e7
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-01-11 23:48:34 +0900

    Update SignUpActivity.kt

--- app/src/main/java/com/example/sowoon/SignUpActivity.kt
+++ app/src/main/java/com/example/sowoon/SignUpActivity.kt
@@ -4,25 +4,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.example.sowoon.data.entity.User
-import com.example.sowoon.database.Database
+import com.example.sowoon.database.AppDatabase
 import com.example.sowoon.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
 
     lateinit var binding: ActivitySignUpBinding
-    lateinit var database: Database
+    lateinit var database: AppDatabase
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.signupBtn.setOnClickListener {
+            signUp()
+        }
     }
 
 
 
     private fun signUp(){
-        database = Database.getInstance(applicationContext)!!
+        database = AppDatabase.getInstance(this)!!
         var email = binding.idEt.text.toString()
         var password = binding.passwordEt.text.toString()
         var passwordCheck = binding.passwordcheckEt.text.toString()
