415f437215e4ae058fdbe775d5ff382384d789b4
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-01-11 23:48:32 +0900

    Update Database.kt

--- app/src/main/java/com/example/sowoon/database/Database.kt
+++ app/src/main/java/com/example/sowoon/database/Database.kt
@@ -7,20 +7,20 @@ import androidx.room.RoomDatabase
 import com.example.sowoon.data.entity.User
 
 @Database(entities = [User::class], version = 1)
-abstract class Database(): RoomDatabase() {
+abstract class AppDatabase(): RoomDatabase() {
 
     abstract fun userDao(): UserDao
 
     companion object {
-        private var instance: com.example.sowoon.database.Database? = null
+        private var instance: AppDatabase? = null
 
         @Synchronized
-        fun getInstance(context: Context): com.example.sowoon.database.Database? {
+        fun getInstance(context: Context): AppDatabase? {
             if(instance == null){
-                synchronized(com.example.sowoon.database.Database::class){
+                synchronized(AppDatabase::class){
                     instance = Room.databaseBuilder(
                         context.applicationContext,
-                        com.example.sowoon.database.Database::class.java,
+                        AppDatabase::class.java,
                         "database"
                     ).allowMainThreadQueries().build()
                 }
