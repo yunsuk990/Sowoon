8e16b8f094e84f58d1f77bce20f0fbf461740584
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-01-15 23:59:55 +0900

    Update User.kt

--- app/src/main/java/com/example/sowoon/data/entity/User.kt
+++ app/src/main/java/com/example/sowoon/data/entity/User.kt
@@ -8,11 +8,7 @@ data class User(
     var email: String,
     var name: String,
     var age: String,
-    val school: String?,
-    val awards: String?,
     var password: String,
     //Type Converter 적용
-    val artwork: ArrayList<Gallery>? = null,
-    val bestArtwork: Gallery? = null,
     @PrimaryKey(autoGenerate = true) var id: Int = 0
 )
