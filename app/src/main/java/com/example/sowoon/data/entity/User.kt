88c835d5767c4150bd131a0fc01b0af926920a65
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-01-18 23:54:56 +0900

    Update User.kt

--- app/src/main/java/com/example/sowoon/data/entity/User.kt
+++ app/src/main/java/com/example/sowoon/data/entity/User.kt
@@ -1,9 +1,19 @@
 package com.example.sowoon.data.entity
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "UserTable")
+//일반 유저
+@Entity(tableName = "UserTable",
+    foreignKeys = arrayOf(
+        ForeignKey(
+            entity = Profile::class,
+            parentColumns = arrayOf("userId"),
+            childColumns = arrayOf("id"),
+            onDelete = ForeignKey.CASCADE
+        )
+    ))
 data class User(
     var email: String,
     var name: String,
