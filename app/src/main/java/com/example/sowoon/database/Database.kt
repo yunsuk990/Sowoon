d4a01eef594e1c985be676145598bdac6357a855
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-01-19 23:51:44 +0900

    Update Database.kt

--- app/src/main/java/com/example/sowoon/database/Database.kt
+++ app/src/main/java/com/example/sowoon/database/Database.kt
@@ -4,13 +4,18 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.sowoon.data.entity.Gallery
+import com.example.sowoon.data.entity.Profile
 import com.example.sowoon.data.entity.User
 
-@Database(entities = [User::class], version = 2)
+@Database(entities = [User::class, Profile::class, Gallery::class], version = 10)
+@TypeConverters(RoomConverter::class)
 abstract class AppDatabase(): RoomDatabase() {
 
     abstract fun userDao(): UserDao
     abstract fun profileDao(): ProfileDao
+    abstract fun galleryDao(): GalleryDao
 
     companion object {
         private var instance: AppDatabase? = null
