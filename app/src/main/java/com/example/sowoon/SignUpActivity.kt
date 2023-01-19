9969f546087b54c378eb1f40ca63ae184a5277bc
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-01-19 23:52:34 +0900

    Update SignUpActivity.kt

--- app/src/main/java/com/example/sowoon/SignUpActivity.kt
+++ app/src/main/java/com/example/sowoon/SignUpActivity.kt
@@ -30,11 +30,12 @@ class SignUpActivity : AppCompatActivity() {
         database = AppDatabase.getInstance(this)!!
         var email = binding.idEt.text.toString()
         var password = binding.passwordEt.text.toString()
+        var name = binding.nameEt.text.toString()
         var passwordCheck = binding.passwordcheckEt.text.toString()
         var age = binding.ageEt.text.toString()
 
         if(database.userDao().getUser(email, password) == null){
-            database.userDao().insertUser(User(email, binding.nameEt.text.toString(), age, password))
+            database.userDao().insertUser(User(email, name, age, password))
             startActivity(Intent(applicationContext, LoginActivity::class.java))
         }else{
             Toast.makeText(applicationContext, "이미 가입되어 있는 계정입니다.", Toast.LENGTH_SHORT).show()
