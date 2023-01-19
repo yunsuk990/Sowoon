d2f279f09f79e9ebd892d68839d2f7a6058cde83
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-01-19 23:51:15 +0900

    Update Profile.kt

--- app/src/main/java/com/example/sowoon/data/entity/Profile.kt
+++ app/src/main/java/com/example/sowoon/data/entity/Profile.kt
@@ -1,15 +1,25 @@
 package com.example.sowoon.data.entity
 
+import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
 //화가 등록 유저
-@Entity(tableName = "ProfileTable")
+@Entity(tableName = "ProfileTable",
+    foreignKeys = arrayOf(
+        ForeignKey(
+            entity = User::class,
+            parentColumns = arrayOf("id"),
+            childColumns = arrayOf("userId"),
+            onDelete = ForeignKey.CASCADE
+        )
+    ))
 data class Profile(
-    @PrimaryKey var userId: Int,
-    val school: String?,
-    val awards: String?,
-    val artwork: ArrayList<Gallery>? = null,
-    val bestArtwork: Gallery? = null
+    var school: String? = "",
+    var awards: String? = "",
+    var name: String? = "",
+    var artwork: ArrayList<Gallery>? = ArrayList(),
+    var bestArtwork: Int? = null,
+    @PrimaryKey var userId: Int
 )
