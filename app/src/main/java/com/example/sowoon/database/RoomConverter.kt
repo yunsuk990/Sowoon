3ec1f15eea679d00e39fb85bdbf94879b22757d4
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-01-19 23:51:37 +0900

    Update RoomConverter.kt

--- app/src/main/java/com/example/sowoon/database/RoomConverter.kt
+++ app/src/main/java/com/example/sowoon/database/RoomConverter.kt
@@ -1,11 +1,31 @@
 package com.example.sowoon.database
 
 import androidx.room.TypeConverter
+import com.example.sowoon.data.entity.Gallery
+import com.google.gson.Gson
 
 class RoomConverter {
+    @TypeConverter
+    fun listToJson(value: ArrayList<Gallery>?): String? {
+        return Gson().toJson(value)
+    }
 
     @TypeConverter
+    fun jsonToList(value: String?): ArrayList<Gallery>? {
+        if(value != null){
+            return Gson().fromJson(value, Array<Gallery>::class.java).toList() as ArrayList<Gallery>
+        }
+        return null
+    }
 
-
+//    @TypeConverter
+//    fun galleryToJson(value: Gallery?): String? {
+//        return Gson().toJson(value)
+//    }
+//
+//    @TypeConverter
+//    fun jsonGalleryToList(value: String?): Gallery? {
+//        return Gson().fromJson(value, Gallery::class.java)
+//    }
 
 }
