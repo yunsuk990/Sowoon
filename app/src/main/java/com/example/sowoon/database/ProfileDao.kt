3193133ce3ea696d0aef01905ef96fea33ed6f75
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-01-19 23:51:20 +0900

    Update ProfileDao.kt

--- app/src/main/java/com/example/sowoon/database/ProfileDao.kt
+++ app/src/main/java/com/example/sowoon/database/ProfileDao.kt
@@ -4,7 +4,6 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import com.example.sowoon.data.entity.Profile
-import com.example.sowoon.data.entity.User
 
 @Dao
 interface ProfileDao {
@@ -15,8 +14,10 @@ interface ProfileDao {
     @Query("SELECT * FROM ProfileTable WHERE userId=:userId")
     fun getProfile(userId: Int): Profile?
 
-    @Query("SELECT * FROM ProfileTable")
-    fun getAllProfile(): ArrayList<Profile>?
+    @Query("SELECT * FROM ProfileTable ")
+    fun getAllProfile(): List<Profile>?
+
+
 
 
 }
