c8b603351b35207e03cbb91460c9f7c2a13839f3
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-01-31 23:59:49 +0900

    Update GalleryFragment.kt

--- app/src/main/java/com/example/sowoon/GalleryFragment.kt
+++ app/src/main/java/com/example/sowoon/GalleryFragment.kt
@@ -1,5 +1,6 @@
 package com.example.sowoon
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -35,7 +36,7 @@ class GalleryFragment : Fragment() {
         binding.addGallery.setOnClickListener {
             //나중에 앨범에서 사진 가져오기
             if(jwt != 0){
-                addGallery()
+                startActivity(Intent(context, AddGalleryActivity::class.java))
             }else{
                 Toast.makeText(context, "로그인 후 이용 가능합니다.", Toast.LENGTH_SHORT).show()
             }
