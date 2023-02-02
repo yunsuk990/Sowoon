a6c0a1aa567ec49e27cf95ffb3eccbcdff5d1a1a
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-02-02 23:25:43 +0900

    Update RoomConverter.kt

--- app/src/main/java/com/example/sowoon/database/RoomConverter.kt
+++ app/src/main/java/com/example/sowoon/database/RoomConverter.kt
@@ -6,31 +6,31 @@ import com.google.gson.Gson
 
 class RoomConverter {
     @TypeConverter
-    fun listToJson(value: List<Gallery>?): String? {
+    fun listToJson(value: ArrayList<Gallery>?): String? {
         if (value == null) return null
         return Gson().toJson(value)
     }
 
     @TypeConverter
-    fun jsonToList(value: String?): List<Gallery>? {
+    fun jsonToList(value: String?): ArrayList<Gallery>? {
         if(value == null) return null
-        return Gson().fromJson(value, Array<Gallery>::class.java) as? List<Gallery>
+        return Gson().fromJson(value, Array<Gallery>::class.java) as? ArrayList<Gallery>
     }
 
 
     @TypeConverter
-    fun fromListIntToString(intList: ArrayList<Int>? = null): String? {
+    fun fromListIntToString(intList: ArrayList<String>? = null): String? {
         if(intList == null) return null
         return intList.toString()
     }
     @TypeConverter
-    fun toListIntFromString(stringList: String? = null): ArrayList<Int>? {
+    fun toListIntFromString(stringList: String? = null): ArrayList<String>? {
         if(stringList == null) return null
-        val result = ArrayList<Int>()
+        val result = ArrayList<String>()
         val split = stringList!!.replace("[","").replace("]","").replace(" ","").split(",")
         for (n in split) {
             try {
-                result.add(n.toInt())
+                result.add(n)
             } catch (e: Exception) {
 
             }
