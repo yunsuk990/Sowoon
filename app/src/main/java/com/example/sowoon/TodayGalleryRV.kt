d1c7eaf514e441010a3a6ff14217a3d4fe64b291
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-02-02 23:26:25 +0900

    Update TodayGalleryRV.kt

--- app/src/main/java/com/example/sowoon/TodayGalleryRV.kt
+++ app/src/main/java/com/example/sowoon/TodayGalleryRV.kt
@@ -1,12 +1,23 @@
 package com.example.sowoon
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
+import android.provider.MediaStore.Audio.Media
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.net.toUri
 import androidx.recyclerview.widget.RecyclerView
 import com.example.sowoon.data.entity.Gallery
 import com.example.sowoon.databinding.ItemTodayalbumBinding
+import kotlin.coroutines.coroutineContext
 
-class TodayGalleryRV: RecyclerView.Adapter<TodayGalleryRV.ViewHolder>() {
+class TodayGalleryRV(var context: Context): RecyclerView.Adapter<TodayGalleryRV.ViewHolder>() {
 
     private var galleryList = ArrayList<Gallery>()
 
@@ -29,7 +40,14 @@ class TodayGalleryRV: RecyclerView.Adapter<TodayGalleryRV.ViewHolder>() {
 
     inner class ViewHolder(val binding: ItemTodayalbumBinding): RecyclerView.ViewHolder(binding.root){
         fun bind(gallery: Gallery){
-            binding.todayAlbumIv.setImageResource(gallery.GalleryId!!)
+            var uri = gallery.GalleryId.toUri()
+            var bitmap: Bitmap = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                ImageDecoder.decodeBitmap(ImageDecoder.createSource(context.contentResolver, uri))
+            } else {
+                MediaStore.Images.Media.getBitmap(context.contentResolver, uri)
+            }
+            Log.d("galleryuri", uri.toString())
+            binding.todayAlbumIv.setImageBitmap(bitmap)
             binding.todayAlbumTitle.text = gallery.title
             binding.todayAlbumArtist.text = gallery.artist
             binding.todayAlbumInfo.text= gallery.info
