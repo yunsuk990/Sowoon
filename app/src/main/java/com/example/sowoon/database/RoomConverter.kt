0f684dd81eca28936ab22fc3022422b1f1f699db
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-02-04 23:56:19 +0900

    Update RoomConverter.kt

--- app/src/main/java/com/example/sowoon/database/RoomConverter.kt
+++ app/src/main/java/com/example/sowoon/database/RoomConverter.kt
@@ -5,14 +5,15 @@ import com.example.sowoon.data.entity.Gallery
 import com.google.gson.Gson
 
 class RoomConverter {
+
     @TypeConverter
-    fun listToJson(value: ArrayList<Gallery>?): String? {
+    fun listToJson(value: ArrayList<Gallery>? = null): String? {
         if (value == null) return null
         return Gson().toJson(value)
     }
 
     @TypeConverter
-    fun jsonToList(value: String?): ArrayList<Gallery>? {
+    fun jsonToList(value: String? = null): ArrayList<Gallery>? {
         if(value == null) return null
         return Gson().fromJson(value, Array<Gallery>::class.java) as? ArrayList<Gallery>
     }
@@ -38,6 +39,25 @@ class RoomConverter {
         return result
     }
 
+    @TypeConverter
+    fun ListIntToString(intList: ArrayList<Int>? = null): String? {
+        if(intList == null) return null
+        return intList.toString()
+    }
+    @TypeConverter
+    fun ListIntFromString(stringList: String? = null): ArrayList<Int>? {
+        if(stringList == null) return null
+        val result = ArrayList<Int>()
+        val split = stringList!!.replace("[","").replace("]","").replace(" ","").split(",")
+        for (n in split) {
+            try {
+                result.add(n.toInt())
+            } catch (e: Exception) {
+
+            }
+        }
+        return result
+    }
 
 //    @TypeConverter
 //    fun galleryToJson(value: Gallery?): String? {
