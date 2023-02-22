333f3598fd5af19b042ad067158348ee9b484277
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-02-22 23:58:42 +0900

    Update ArtistProfileFragment.kt

--- app/src/main/java/com/example/sowoon/ArtistProfileFragment.kt
+++ app/src/main/java/com/example/sowoon/ArtistProfileFragment.kt
@@ -1,11 +1,15 @@
 package com.example.sowoon
 
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.example.sowoon.data.entity.Profile
 import com.example.sowoon.data.entity.User
 import com.example.sowoon.database.AppDatabase
@@ -17,6 +21,7 @@ class ArtistProfileFragment : Fragment() {
     lateinit var binding: FragmentArtistProfileBinding
     private var gson = Gson()
     lateinit var database: AppDatabase
+    var uri: Uri? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,9 +31,9 @@ class ArtistProfileFragment : Fragment() {
         binding = FragmentArtistProfileBinding.inflate(inflater, container, false)
         database = AppDatabase.getInstance(requireContext())!!
         val profileJson = arguments?.getString("profile")
+        uri = arguments?.getString("profileImage")?.toUri()
         val profile = gson.fromJson(profileJson, Profile::class.java)
         setProfile(profile)
-
         return binding.root
     }
 
@@ -39,6 +44,7 @@ class ArtistProfileFragment : Fragment() {
         binding.artistsProfileSchoolInput.text = profile.school
         binding.artistsProfileAwardsInput.text = profile.awards
         binding.artistsProfileAgeInput.text = age.toString()
+        Glide.with(requireContext()).load(uri).into(binding.artistsProfileIv)
     }
 
     private fun User(): User? {
