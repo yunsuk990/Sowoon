764a5e6562bede72b27627b57b30ba8565acdff4
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-03-07 23:59:15 +0900

    Create MessageMenu.kt

---
+++ app/src/main/java/com/example/sowoon/message/MessageMenu.kt
@@ -0,0 +1,12 @@
+package com.example.sowoon.message
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sowoon.R
+
+class MessageMenu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_message_menu)
+    }
+}
