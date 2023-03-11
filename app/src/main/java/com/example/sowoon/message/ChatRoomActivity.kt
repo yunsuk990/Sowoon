83e47acb872f6c432721ef7e7c2773b2a61a53de
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-03-11 17:27:14 +0900

    Update ChatRoomActivity.kt

--- app/src/main/java/com/example/sowoon/message/ChatRoomActivity.kt
+++ app/src/main/java/com/example/sowoon/message/ChatRoomActivity.kt
@@ -2,6 +2,7 @@ package com.example.sowoon.message
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import com.example.sowoon.R
 import com.example.sowoon.data.entity.ChatModel
@@ -14,31 +15,35 @@ import com.google.firebase.ktx.Firebase
 class ChatRoomActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityChatRoomBinding
-    var getJwt = getJwt()
     lateinit var firebaseDB: FirebaseFirestore
+    private var chatRoomUid: String? = null
+    private var destUid: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityChatRoomBinding.inflate(layoutInflater)
         var artist = intent.getStringExtra("Artist")
-        var destUid = intent.getStringExtra("userId")
+        destUid = intent.getStringExtra("userId") //채팅 상대방
+        Log.d("destUid", destUid.toString())
+
         var firebaseDB = Firebase.firestore
         binding.chatRoomSendTitleTv.text = artist + "님에게 메세지"
         setContentView(binding.root)
 
-        if(getJwt != 0){
+        if(getJwt() != 0){
             binding.chatRoomSendBtn.setOnClickListener{ view ->
                 var chatModel: ChatModel = ChatModel()
-                chatModel.users?.put(getJwt!!, true)
-                chatModel.users?.put(destUid?.toInt()!!, true)
+                chatModel.users?.put(destUid!!,true)
+                chatModel.users?.put(getJwt().toString(), true)
 
                 if(chatRoomUid == null){
-                    Toast.makeText(this, "채팅방 생성", Toast.LENGTH_SHORT).show()
                     binding.chatRoomSendBtn.isEnabled = false
                     firebaseDB.collection("chatrooms").add(chatModel).addOnSuccessListener{
                         checkChatRoom()
                     }
+                    Log.d("ChatRoom", "성공")
                 }else{
+                    Log.d("ChatRoom", "실패")
                     sendMsgDatabase()
                 }
             }
@@ -49,17 +54,17 @@ class ChatRoomActivity : AppCompatActivity() {
 
 
     private fun checkChatRoom(){
-
+//        firebaseDB.collection("chatrooms")
     }
 
     private fun sendMsgDatabase(){
         if(!binding.chatRoomSendEt.text.toString().equals("")){
-            
+
         }
     }
 
     private fun getJwt(): Int? {
-        val spf = getSharedPreferences("auth", AppCompatActivity.MODE_PRIVATE)
+        val spf = getSharedPreferences("auth", MODE_PRIVATE)
         var jwt = spf?.getInt("jwt", 0)
         return jwt
     }
