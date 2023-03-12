4e28a1b5cc3e954559e66d1bbe91e5f0074993ac
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-03-12 22:42:29 +0900

    Update ChatModel.kt

--- app/src/main/java/com/example/sowoon/data/entity/ChatModel.kt
+++ app/src/main/java/com/example/sowoon/data/entity/ChatModel.kt
@@ -4,13 +4,12 @@ import java.util.Objects
 
 class ChatModel {
 
-    var users: MutableMap<String, Boolean>? = HashMap() //채팅방 유저들
-    var comments: Map<String, Comment> = HashMap() //채팅방의 내용
+    var users: MutableMap<String, Boolean> = HashMap() //채팅방 유저들
+    var comments: MutableMap<String, Comment> = HashMap() //채팅방의 내용
 
     class Comment {
+        var message: String? = null
         var userId: Int? = 0
-        var message: String? = ""
-        var timestamp: Objects? = null
     }
 
 }
