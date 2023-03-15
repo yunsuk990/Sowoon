97c50ed33086e507cdd26c96c7657b947ae30972
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-03-15 18:17:15 +0900

    Update ChatRoomActivity.kt

--- app/src/main/java/com/example/sowoon/message/ChatRoomActivity.kt
+++ app/src/main/java/com/example/sowoon/message/ChatRoomActivity.kt
@@ -18,6 +18,8 @@ import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.getValue
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 
 class ChatRoomActivity : AppCompatActivity() {
 
@@ -34,59 +36,63 @@ class ChatRoomActivity : AppCompatActivity() {
         binding = ActivityChatRoomBinding.inflate(layoutInflater)
         setContentView(binding.root)
         database = AppDatabase.getInstance(this)!!
-        destUid = intent.getStringExtra("userId") //채팅 상대방
+        destUid = intent.getStringExtra("userId").toString() //채팅을 당하는 유저 jwt
         var artist = database.userDao().getUserProfile(destUid!!.toInt()).name
         firebaseDB = FirebaseDatabase.getInstance()
         binding.chatRoomSendTitleTv.text = artist+ " 작가"
         binding.backBtn.setOnClickListener {
             finish()
-            startActivity(Intent(this, MessageMenu::class.java))
+            var intent = Intent(this, MessageMenu::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+            startActivity(intent)
         }
 
         if(getJwt() != 0){
-            chatRoomSend()
-            checkChatRoom()
-        }
-    }
+            binding.chatRoomSendBtn.setOnClickListener {
+                var chatModel = ChatModel()
+                chatModel.users.add(destUid.toString())
+                chatModel.users.add(getJwt().toString())
+                Log.d("user", chatModel.users.toString())
 
-    //메세지 보내기
-    private fun chatRoomSend() {
-        binding.chatRoomSendBtn.setOnClickListener { view ->
-            var chatModel: ChatModel = ChatModel()
-            chatModel.users?.put(destUid!!, true)
-            chatModel.users?.put(getJwt().toString(), true)
-
-            if (chatRoomUid == null) {
-                binding.chatRoomSendBtn.isEnabled = false //요청 가기도 전에 버튼 누르는 것을 방지
-                firebaseDB.getReference().child("chatrooms").push().setValue(chatModel)
-                    .addOnSuccessListener(object : OnSuccessListener<Void> {
-                        override fun onSuccess(p0: Void?) {
+                if (chatRoomUid == null) {
+                    binding.chatRoomSendBtn.isEnabled = false //요청 가기도 전에 버튼 누르는 것을 방지
+                    firebaseDB.getReference().child("chatrooms").push().setValue(chatModel)
+                        .addOnSuccessListener {
                             checkChatRoom()
                         }
-                    })
-            } else {
-                var comment: ChatModel.Comment = ChatModel.Comment()
-                comment.userId = getJwt()
-                comment.message = binding.chatRoomSendEt.text.toString()
-                comment.timestamp = time.format(Date())
-                firebaseDB.getReference().child("chatrooms").child(chatRoomUid!!).child("comments")
-                    .push().setValue(comment).addOnCompleteListener {
-                        binding.chatRoomSendEt.setText("")
-                    }
+                } else {
+                    var comment: ChatModel.Comment = ChatModel.Comment()
+                    comment.userId = getJwt()
+                    comment.message = binding.chatRoomSendEt.text.toString()
+                    comment.timestamp = time.format(Date())
+                    firebaseDB.getReference().child("chatrooms").child(chatRoomUid!!).child("comments")
+                        .push().setValue(comment).addOnCompleteListener {
+                            binding.chatRoomSendEt.setText("")
+                        }
+                }
             }
+            checkChatRoom()
+
         }
     }
-
 
     //유효성 검사
     private fun checkChatRoom(){
         Log.d("checkChatRoom", "Start")
-        firebaseDB.getReference().child("chatrooms").orderByChild("users/${getJwt()}").equalTo(true)
+        firebaseDB.getReference().child("chatrooms").orderByChild("users/${getJwt()}")
             .addListenerForSingleValueEvent(object: ValueEventListener{
                 override fun onDataChange(snapshot: DataSnapshot) {
+                    Log.d("item", snapshot.children.toString())
                     for(item in snapshot.children){
-                        var chatModel: ChatModel? = item.getValue<ChatModel>()
-                        if(chatModel?.users?.contains(destUid)!!){
+                        Log.d("desUid", destUid.toString())
+
+
+                        //ChatModel.user 를 Map으로 정의해도 가져올떄 배열로 가져와서 오류
+                        var chatModel: ChatModel = item.getValue<ChatModel>()!!
+                        Log.d("array", chatModel.users.toString())
+
+
+                        if(chatModel.users?.contains(destUid)!!){
                             chatRoomUid = item.key
                             binding.chatRoomSendBtn.isEnabled = true
 
