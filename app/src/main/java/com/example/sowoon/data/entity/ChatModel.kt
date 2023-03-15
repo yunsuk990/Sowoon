cb475c9e6a9a9533b8f38603058018958954516d
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-03-15 18:17:13 +0900

    Update ChatModel.kt

--- app/src/main/java/com/example/sowoon/data/entity/ChatModel.kt
+++ app/src/main/java/com/example/sowoon/data/entity/ChatModel.kt
@@ -1,11 +1,8 @@
 package com.example.sowoon.data.entity
 
-import com.google.firebase.database.ServerValue
-import java.util.Objects
-
 class ChatModel {
 
-    var users: MutableMap<String, Boolean> = HashMap() //채팅방 유저들
+    var users: ArrayList<String> = ArrayList()
     var comments: MutableMap<String, Comment> = HashMap() //채팅방의 내용
 
     class Comment {
