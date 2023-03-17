a5e279f6ec84b003a01e03b88a2aee8a30784310
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-03-17 16:32:39 +0900

    Create ProfileModel.kt

---
+++ app/src/main/java/com/example/sowoon/data/entity/ProfileModel.kt
@@ -0,0 +1,10 @@
+package com.example.sowoon.data.entity
+
+import androidx.room.PrimaryKey
+
+class ProfileModel {
+    var school: String? = null
+    var awards: String? = null
+    var profileImg: String? = null
+    var bestArtwork: String? = null
+}
