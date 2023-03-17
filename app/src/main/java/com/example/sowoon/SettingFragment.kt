d06d98979fcf68413b2d750506986a8c461dc79c
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-03-17 16:33:00 +0900

    Update SettingFragment.kt

--- app/src/main/java/com/example/sowoon/SettingFragment.kt
+++ app/src/main/java/com/example/sowoon/SettingFragment.kt
@@ -1,5 +1,6 @@
 package com.example.sowoon
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -10,9 +11,18 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.sowoon.data.entity.User
+import com.example.sowoon.data.entity.UserModel
 import com.example.sowoon.database.AppDatabase
 import com.example.sowoon.databinding.FragmentSettingBinding
 import com.example.sowoon.message.MessageMenu
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.ktx.storage
@@ -23,9 +33,10 @@ class SettingFragment : Fragment() {
     lateinit var binding: FragmentSettingBinding
     lateinit var database: AppDatabase
     lateinit var gson: Gson
-    var user: User? = null
-    var jwt: Int = -1
     lateinit var storage: FirebaseStorage
+    lateinit var firebaseAuth: FirebaseAuth
+    lateinit var firebaseDatabase: FirebaseDatabase
+    var currentUser: FirebaseUser? = null
 
 
     override fun onCreateView(
@@ -36,15 +47,22 @@ class SettingFragment : Fragment() {
         binding = FragmentSettingBinding.inflate(inflater, container, false)
         database = AppDatabase.getInstance(requireContext())!!
         storage = Firebase.storage
-        jwt = getJwt()!!
-        user = User()
+        firebaseAuth = FirebaseAuth.getInstance()
+        firebaseDatabase = FirebaseDatabase.getInstance()
         initClickListener()
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if(firebaseAuth.currentUser != null){
+            currentUser = firebaseAuth.currentUser!!
+        }
+    }
+
     private fun initClickListener(){
         binding.settingLoginTv.setOnClickListener {
-            if(jwt != 0){
+            if(currentUser != null){
                 Toast.makeText(context, "이미 로그인되어있습니다.", Toast.LENGTH_SHORT).show()
             }else{
                 startActivity(Intent(context, LoginActivity::class.java))
@@ -52,7 +70,7 @@ class SettingFragment : Fragment() {
         }
 
         binding.settingSignupTv.setOnClickListener {
-            if(jwt != 0){
+            if(currentUser != null){
                 Toast.makeText(context, "이미 로그인되어있습니다.", Toast.LENGTH_SHORT).show()
             }else {
                 startActivity(Intent(context, SignUpActivity::class.java))
@@ -64,7 +82,7 @@ class SettingFragment : Fragment() {
         }
 
         binding.settingLogoutTv.setOnClickListener {
-            if(jwt != 0){
+            if(currentUser != null){
                 logOut()
             }else{
                 Toast.makeText(context, "로그인 후 이용하시기 바랍니다.", Toast.LENGTH_SHORT).show()
@@ -72,7 +90,7 @@ class SettingFragment : Fragment() {
         }
 
         binding.settingMyInfoTv.setOnClickListener {
-            if(jwt != 0){
+            if(currentUser != null){
                 (context as MainActivity).supportFragmentManager.beginTransaction()
                     .replace(R.id.main_frame, SettingMyInfoFragment())
                     .commitNowAllowingStateLoss()
@@ -82,14 +100,23 @@ class SettingFragment : Fragment() {
         }
 
         binding.settingSignupArtistTv.setOnClickListener {
-            if(jwt != 0){
-                user = database.userDao().getUser(user!!.email, user!!.password)!!
-                Log.d("ifArtist", user?.ifArtist!!.toString())
-                if(user?.ifArtist!!){
-                    Toast.makeText(context, "이미 화가 등록 되어있습니다.",Toast.LENGTH_SHORT).show()
-                }else{
-                    startActivity(Intent(activity, RegistArtistActivity::class.java))
-                }
+            if(currentUser != null){
+                firebaseDatabase.getReference().child("users").child(currentUser!!.uid).addListenerForSingleValueEvent(
+                    object: ValueEventListener {
+                        override fun onDataChange(snapshot: DataSnapshot) {
+                                var userModel = snapshot.getValue(UserModel::class.java)!!
+                                if (userModel.ifArtist) {
+                                    Toast.makeText(context, "이미 화가 등록 되어있습니다.", Toast.LENGTH_SHORT)
+                                        .show()
+                                } else {
+                                    startActivity(Intent(activity,
+                                        RegistArtistActivity::class.java))
+                                }
+
+                        }
+
+                        override fun onCancelled(error: DatabaseError) {}
+                    })
             }else{
                 Toast.makeText(context, "로그인 후 이용하시기 바랍니다.", Toast.LENGTH_SHORT).show()
             }
@@ -97,10 +124,11 @@ class SettingFragment : Fragment() {
 
         //회원탈퇴
         binding.settingQuitTv.setOnClickListener {
-            if(jwt != 0){
-                database.userDao().deleteUser(User()!!)
+            if(currentUser != null){
+                //database.userDao().deleteUser(User()!!)
                 removeJwt()
-                var use = database.userDao().getUser(user!!.email, user!!.password)
+                firebaseAuth.currentUser?.delete()
+//                var use = database.userDao().getUser(user!!.email, user!!.password)
                 //회원탈퇴시 좋아요 매커니즘 어떻게 할 것인지
 //                if (likeGallery != null) {
 //                    for( i in likeGallery){
@@ -109,8 +137,8 @@ class SettingFragment : Fragment() {
 //                        database.galleryDao().setlikeCount(i, likecount)
 //                    }
 //                }
-                database.galleryDao().deleteUserGallery(jwt)
-                val desertRef = storage.reference.child("images/"+jwt)
+                database.galleryDao().deleteUserGallery(getJwt()!!.toInt())
+                val desertRef = storage.reference.child("images/"+getJwt()!!.toInt())
                 desertRef.delete().addOnSuccessListener {
                     Log.d("DELETE", "SUCCESS")
                 }.addOnFailureListener{
@@ -124,8 +152,8 @@ class SettingFragment : Fragment() {
     }
 
     private fun logOut(){
-        val user = getJwt()
-        if( user != 0){
+        if(currentUser != null){
+            firebaseAuth.signOut()
             removeJwt()
             Toast.makeText(context, "로그아웃", Toast.LENGTH_SHORT).show()
             startActivity(Intent(context, MainActivity::class.java))
@@ -143,12 +171,5 @@ class SettingFragment : Fragment() {
         val editor = spf!!.edit()
         editor.remove("jwt")
         editor.apply()
-    }
-
-    private fun User(): User? {
-        gson = Gson()
-        val spf =
-            requireActivity().getSharedPreferences("userProfile", AppCompatActivity.MODE_PRIVATE)
-        return gson.fromJson(spf.getString("user", null), User::class.java)
     }
 }
