d0b6711469ac2abe2892d131c524818ea0f4e442
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-03-17 16:32:57 +0900

    Update RegistArtistActivity.kt

--- app/src/main/java/com/example/sowoon/RegistArtistActivity.kt
+++ app/src/main/java/com/example/sowoon/RegistArtistActivity.kt
@@ -11,12 +11,20 @@ import android.widget.Toast
 import androidx.core.net.toUri
 import com.bumptech.glide.Glide
 import com.example.sowoon.data.entity.Profile
+import com.example.sowoon.data.entity.ProfileModel
 import com.example.sowoon.data.entity.User
 import com.example.sowoon.database.AppDatabase
 import com.example.sowoon.databinding.ActivityRegistArtistBinding
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.UploadTask
 import com.google.firebase.storage.ktx.storage
 import com.google.gson.Gson
 
@@ -25,10 +33,13 @@ class RegistArtistActivity : AppCompatActivity() {
     lateinit var binding: ActivityRegistArtistBinding
     lateinit var database: AppDatabase
     lateinit var storage: FirebaseStorage
+    lateinit var firebaseAuth: FirebaseAuth
+    lateinit var firebaseDatabase: FirebaseDatabase
     lateinit var gson: Gson
-    var GalleryId: String = ""
+    var bestArtwork: String = ""
     val REQ_GALLERY = 10
     var URI: Uri? = null
+    var currentUser: FirebaseUser? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,7 +47,10 @@ class RegistArtistActivity : AppCompatActivity() {
         setContentView(binding.root)
         storage = Firebase.storage
         database = AppDatabase.getInstance(this)!!
-        binding.myInfoName.text = getName()
+        firebaseAuth = FirebaseAuth.getInstance()
+        firebaseDatabase = FirebaseDatabase.getInstance()
+
+        //binding.myInfoName.text = getName()
 
         binding.myInfoIv.setOnClickListener{
             openGallery()
@@ -50,48 +64,50 @@ class RegistArtistActivity : AppCompatActivity() {
         }
     }
 
+    override fun onStart() {
+        super.onStart()
+        currentUser = firebaseAuth.currentUser
+    }
+
     private fun registInfo(){
-        var jwt = getJwt()
-        if(jwt == 0){
+        if(currentUser == null){
             Toast.makeText(this, "로그인 후 이용해주시기 바랍니다." ,Toast.LENGTH_SHORT).show()
             return
         }
         var school = binding.myInfoSchoolInput.text.toString()
         var awards = binding.myInfoAwardsInput.text.toString()
-        var bestArtwork: String? = null
-        var profileImg: String? = null
 
         Toast.makeText(this, "등록되었습니다." ,Toast.LENGTH_SHORT).show()
 
-        if(GalleryId != ""){
-            var gallery = database.galleryDao().getGallery(GalleryId)
-            var galleryPath = gallery.galleryPath
-            bestArtwork = galleryPath
-        }
+//        if(bestArtwork != ""){
+//            //var gallery = database.galleryDao().getGallery(bestArtwork.toString())
+//            //var galleryPath = gallery.galleryPath
+//            //bestArtwork = galleryPath
+//        }
+
         if(URI != null){
-            var mountainImageRef: StorageReference? = storage?.reference?.child("images")?.child(getJwt().toString())?.child("Profile")?.child("profile.png")
+            var mountainImageRef: StorageReference? = storage?.reference?.child("profile")?.child(currentUser!!.uid)?.child("profile.png")
             Log.d("FirebaseUri", URI.toString())
             mountainImageRef?.putFile(URI!!)?.addOnSuccessListener {
-                mountainImageRef.downloadUrl.addOnSuccessListener { uri ->
-                    Log.d("registProfileImage", uri.toString())
-                    profileImg = uri.toString()
-                    var profile: Profile = Profile(school,awards,getName(),profileImg ,bestArtwork, jwt!!)
-                    Log.d("profile", profile.toString())
-                    database.userDao().ifArtistRegist(jwt!!,true)
-                    database.profileDao().insertProfile(profile)
+                mountainImageRef.downloadUrl.addOnSuccessListener{ url ->
+                    var profileModel = ProfileModel()
+                    profileModel.profileImg = url.toString()
+                    profileModel.bestArtwork = bestArtwork
+                    profileModel.awards = awards
+                    profileModel.school = school
+                    Log.d("profileImg",url.toString())
+                    var map: MutableMap<String, Any> = HashMap()
+                    map.put("ifArtist", true)
+                    firebaseDatabase.getReference().child("profile").child(currentUser!!.uid).setValue(profileModel)
+                    firebaseDatabase.getReference().child("users").child(currentUser!!.uid).updateChildren(map)
+//                    database.userDao().ifArtistRegist(jwt!!,true)
+//                    database.profileDao().insertProfile(profile)
                 }
-            }?.addOnFailureListener{
-                Log.d("registProfileImage", "FAIL", it)
             }
         }
         finish()
     }
 
-    private fun User(): User? {
-        gson = Gson()
-        val spf = getSharedPreferences("userProfile", MODE_PRIVATE)
-        return gson.fromJson(spf.getString("user", null), User::class.java)
-    }
 
     private fun openGallery(){
         val intent = Intent(Intent.ACTION_PICK)
@@ -99,15 +115,9 @@ class RegistArtistActivity : AppCompatActivity() {
         startActivityForResult(intent, REQ_GALLERY)
     }
 
-    private fun getName(): String?{
-        var use = User()
-        var User = database.userDao().getUser(use!!.email, use!!.password)
-        return User?.name.toString()
-    }
-
-    private fun getJwt(): Int? {
+    private fun getJwt(): String? {
         val spf = getSharedPreferences("auth", MODE_PRIVATE)
-        var jwt = spf?.getInt("jwt", 0)
+        var jwt = spf?.getString("jwt", null)
         return jwt
     }
 
@@ -116,10 +126,10 @@ class RegistArtistActivity : AppCompatActivity() {
         if (resultCode == RESULT_OK) {
             when (requestCode) {
                 0 -> {
-                    GalleryId = data?.getStringExtra("GalleryId")!!
-                    Log.d("GalleryId", GalleryId.toString())
+                    bestArtwork = data?.getStringExtra("BestArtworkURL")!!
+                    Log.d("BestArtworkURL", bestArtwork.toString())
                     binding.myInfoBestArtworkIv.scaleType = ImageView.ScaleType.FIT_XY
-                    Glide.with(this).load(GalleryId).into(binding.myInfoBestArtworkIv)
+                    Glide.with(this).load(bestArtwork).into(binding.myInfoBestArtworkIv)
                     binding.myInfoBestArtworkIv.scaleType = ImageView.ScaleType.FIT_XY
                 }
                 REQ_GALLERY -> {
