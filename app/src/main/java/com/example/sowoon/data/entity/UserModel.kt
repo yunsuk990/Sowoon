de7cb7c17b9733dcc40da48e8274aa9c79d15e69
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-03-17 16:32:37 +0900

    Create UserModel.kt

---
+++ app/src/main/java/com/example/sowoon/data/entity/UserModel.kt
@@ -0,0 +1,11 @@
+package com.example.sowoon.data.entity
+
+class UserModel {
+    var jwt: String? = null
+    var name: String? = null
+    var pushToken: String? = null
+//    var email: String? = null
+    var age: String? = null
+//    var password: String? = null
+    var ifArtist: Boolean = false
+}
