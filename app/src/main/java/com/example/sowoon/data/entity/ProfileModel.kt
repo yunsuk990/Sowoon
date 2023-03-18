5d7a7ea5b0e8760b7c634c17f542c7f7945a51a5
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-03-18 23:54:29 +0900

    Update ProfileModel.kt

--- app/src/main/java/com/example/sowoon/data/entity/ProfileModel.kt
+++ app/src/main/java/com/example/sowoon/data/entity/ProfileModel.kt
@@ -5,6 +5,5 @@ import androidx.room.PrimaryKey
 class ProfileModel {
     var school: String? = null
     var awards: String? = null
-    var profileImg: String? = null
     var bestArtwork: String? = null
 }
