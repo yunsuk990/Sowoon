b69ca4af75bb4a39552e3f9e1d1a622408ea3e94
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-03-18 23:55:00 +0900

    Update SignUpActivity.kt

--- app/src/main/java/com/example/sowoon/SignUpActivity.kt
+++ app/src/main/java/com/example/sowoon/SignUpActivity.kt
@@ -1,8 +1,11 @@
 package com.example.sowoon
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.ImageView
 import android.widget.Toast
 import com.example.sowoon.data.entity.User
 import com.example.sowoon.data.entity.UserModel
@@ -18,6 +21,8 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 
 class SignUpActivity : AppCompatActivity() {
 
@@ -25,6 +30,9 @@ class SignUpActivity : AppCompatActivity() {
     // lateinit var database: AppDatabase //roomDB
     lateinit var firebaseDatabase: FirebaseDatabase
     lateinit var firebaseAuth: FirebaseAuth
+    lateinit var firebaseStorage: FirebaseStorage
+    val REQ_GALLERY = 10
+    var profileURL: Uri? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,7 +40,12 @@ class SignUpActivity : AppCompatActivity() {
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
         firebaseDatabase = FirebaseDatabase.getInstance()
-        firebaseAuth = Firebase.auth
+        firebaseAuth = FirebaseAuth.getInstance()
+        firebaseStorage = FirebaseStorage.getInstance()
+
+        binding.signupProfileIv.setOnClickListener {
+            openGallery()
+        }
 
         binding.signupBtn.setOnClickListener {
             signUp()
@@ -50,14 +63,20 @@ class SignUpActivity : AppCompatActivity() {
         var age = binding.ageEt.text.toString()
 
         if(firebaseAuth.currentUser == null){
+            var mountainImageRef: StorageReference? = firebaseStorage?.reference?.child("images")?.child(profileURL?.lastPathSegment.toString())
             firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(object: OnCompleteListener<AuthResult>{
                 override fun onComplete(p0: Task<AuthResult>) {
-                    var uid = p0.result.user?.uid
-                    var userModel = UserModel()
-                    userModel.jwt = uid.toString()
-                    userModel.name = name
-                    userModel.age = age
-                    firebaseDatabase.getReference().child("users").child(uid.toString()).setValue(userModel)
+                    mountainImageRef?.putFile(profileURL!!)?.addOnSuccessListener {
+                        mountainImageRef.downloadUrl.addOnSuccessListener { url ->
+                            var uid = p0.result.user?.uid
+                            var userModel = UserModel()
+                            userModel.jwt = uid.toString()
+                            userModel.name = name
+                            userModel.age = age
+                            userModel.profileImg = url.toString()
+                            firebaseDatabase.getReference().child("users").child(uid.toString()).setValue(userModel)
+                        }
+                    }
                 }
             }).addOnFailureListener {
                 Toast.makeText(applicationContext, "이미 가입되어 있는 계정입니다.", Toast.LENGTH_SHORT).show()
@@ -65,6 +84,27 @@ class SignUpActivity : AppCompatActivity() {
             startActivity(Intent(applicationContext, LoginActivity::class.java))
         }else{
             Toast.makeText(applicationContext, "이미 로그인되어있습니다.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun openGallery(){
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = MediaStore.Images.Media.CONTENT_TYPE
+        startActivityForResult(intent, REQ_GALLERY)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == RESULT_OK){
+            when(requestCode){
+                REQ_GALLERY -> {
+                    data?.data?.let { uri ->
+                        profileURL = uri
+                        binding.signupProfileIv.setImageURI(uri)
+                        binding.signupProfileIv.scaleType = ImageView.ScaleType.FIT_XY
+                    }
+                }
+            }
         }
     }
 }
