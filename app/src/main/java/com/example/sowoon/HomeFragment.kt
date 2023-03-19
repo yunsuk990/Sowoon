7f9b07e420ae9f6cba7d73566739fdf92d57fd03
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-03-19 15:26:18 +0900

    Update HomeFragment.kt

--- app/src/main/java/com/example/sowoon/HomeFragment.kt
+++ app/src/main/java/com/example/sowoon/HomeFragment.kt
@@ -1,17 +1,24 @@
 package com.example.sowoon
 
 import android.os.Bundle
+import android.provider.ContactsContract.Data
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.sowoon.data.entity.GalleryModel
+import com.example.sowoon.data.entity.reference
 import com.example.sowoon.database.AppDatabase
 import com.example.sowoon.databinding.FragmentMainBinding
 import com.google.android.gms.tasks.OnSuccessListener
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.ListResult
 import com.google.firebase.storage.StorageReference
@@ -22,9 +29,10 @@ class HomeFragment : Fragment() {
 
     lateinit var binding: FragmentMainBinding
     lateinit var database: AppDatabase
+    lateinit var firebaseAuth: FirebaseAuth
     lateinit var firebaseDatabase: FirebaseDatabase
     lateinit var firebaseStorage: FirebaseStorage
-    var currentUser: FirebaseUser? = null
+    private var gson = Gson()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,52 +43,52 @@ class HomeFragment : Fragment() {
         database = AppDatabase.getInstance(requireContext())!!
         firebaseDatabase = FirebaseDatabase.getInstance()
         firebaseStorage = FirebaseStorage.getInstance()
+        firebaseAuth = FirebaseAuth.getInstance()
         initRecyclerView()
         return binding.root
     }
+
 
     private fun initRecyclerView(){
         val todayGalleryAdapter = TodayGalleryRV(requireContext()!!)
         binding.mainTodayAlbumRv.adapter = todayGalleryAdapter
         binding.mainTodayAlbumRv.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL,false )
 
-        //테스트 이미지
-//        val galleryList = database.galleryDao().getAllGallery() as ArrayList<Gallery>
-//        todayGalleryAdapter.addGallery(galleryList!!)
-
-
-        var mountainImageRef: StorageReference? = firebaseStorage?.reference?.child("images")
-        mountainImageRef?.listAll()?.addOnSuccessListener(object: OnSuccessListener<ListResult>{
-            override fun onSuccess(p0: ListResult?) {
-                var galleryList: ArrayList<StorageReference> = ArrayList()
-                for(img in p0?.items!!){
-                    galleryList.add(img)
+        firebaseDatabase.getReference().child("images").addListenerForSingleValueEvent(object: ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                var galleryModelList: ArrayList<DataSnapshot>? = ArrayList()
+                for(item in snapshot.children){
+                    Log.d("item", item.toString())
+                    Log.d("itemkey", item.key.toString())
+                    var galleryModel: GalleryModel? = item.getValue(GalleryModel::class.java)
+                    if (galleryModel != null) {
+                        galleryModelList?.add(item)
+                    }
                 }
-                todayGalleryAdapter.addGallery(galleryList!!)
-                Log.d("galleryList", galleryList.toString())
+                if (galleryModelList != null) {
+                    todayGalleryAdapter.addGallery(galleryModelList)
+                }
             }
+            override fun onCancelled(error: DatabaseError) {}
         })
 
-
         todayGalleryAdapter.setMyItemClickListener(object: TodayGalleryRV.MyItemOnClickListener{
-            override fun galleryClick(gallery: StorageReference) {
+            override fun galleryClick(gallery: DataSnapshot) {
                 galleryOnClick(gallery)
             }
         })
     }
 
-    private fun galleryOnClick(gallery: StorageReference){
+    private fun galleryOnClick(gallery: DataSnapshot){
         var ref = reference(gallery)
         (context as MainActivity).supportFragmentManager.beginTransaction()
             .replace(R.id.main_frame, GalleryInfoFragment().apply {
                 arguments = Bundle().apply {
                    putSerializable("gallery", ref)
+//                    val galleryJson = gson.toJson(gallery)
+//                    putString("gallery", galleryJson)
                 }
             })
             .commitNowAllowingStateLoss()
-    }
-
-    class reference(gallery: StorageReference) : Serializable {
-        var gallery = gallery
     }
 }
