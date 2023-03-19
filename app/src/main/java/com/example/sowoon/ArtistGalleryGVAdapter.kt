8531229327f2a67de6cb95de2c7da4df73003c55
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-03-19 15:26:03 +0900

    Update ArtistGalleryGVAdapter.kt

--- app/src/main/java/com/example/sowoon/ArtistGalleryGVAdapter.kt
+++ app/src/main/java/com/example/sowoon/ArtistGalleryGVAdapter.kt
@@ -1,19 +1,22 @@
 package com.example.sowoon
 
 import android.content.Context
+import android.provider.ContactsContract.Data
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import com.bumptech.glide.Glide
 import com.example.sowoon.data.entity.Gallery
+import com.example.sowoon.data.entity.GalleryModel
 import com.example.sowoon.databinding.ItemArtistgalleryBinding
+import com.google.firebase.database.DataSnapshot
 
-class ArtistGalleryGVAdapter(var galleryList: ArrayList<Gallery>?, var context: Context): BaseAdapter() {
+class ArtistGalleryGVAdapter(var galleryList: ArrayList<DataSnapshot>?, var context: Context): BaseAdapter() {
 
 
     interface MyItemClickListener {
-        fun artworkClick(gallery: Gallery)
+        fun artworkClick(gallery: DataSnapshot)
     }
 
     private lateinit var mItemClickListener: MyItemClickListener
@@ -34,8 +37,8 @@ class ArtistGalleryGVAdapter(var galleryList: ArrayList<Gallery>?, var context: 
 
     override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
         var binding = ItemArtistgalleryBinding.inflate(LayoutInflater.from(p2?.context), p2, false)
-        var galleryUrl = galleryList?.get(p0)?.GalleryId!!
-        Glide.with(context).load(galleryUrl).centerCrop().into(binding.galleryIv)
+        var gallery = galleryList?.get(p0)?.getValue(GalleryModel::class.java)
+        Glide.with(context).load(gallery?.imagePath).centerCrop().into(binding.galleryIv)
         binding.galleryIv.setOnClickListener {
             mItemClickListener.artworkClick(galleryList!![p0])
         }
