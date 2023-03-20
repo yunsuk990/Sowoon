033f98c2474ba78a8574281a5dc9c0b08d14fe06
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-03-20 17:01:19 +0900

    Update ArtistsFragment.kt

--- app/src/main/java/com/example/sowoon/ArtistsFragment.kt
+++ app/src/main/java/com/example/sowoon/ArtistsFragment.kt
@@ -11,14 +11,21 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.room.Database
 import com.example.sowoon.data.entity.Gallery
 import com.example.sowoon.data.entity.Profile
+import com.example.sowoon.data.entity.UserModel
 import com.example.sowoon.database.AppDatabase
 import com.example.sowoon.databinding.FragmentArtistsBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.ktx.Firebase
 import com.google.gson.Gson
 
 class ArtistsFragment : Fragment() {
 
     lateinit var binding: FragmentArtistsBinding
     lateinit var database: AppDatabase
+    lateinit var firebaseDatabase: FirebaseDatabase
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,29 +33,38 @@ class ArtistsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentArtistsBinding.inflate(inflater, container, false)
-        database = AppDatabase.getInstance(requireContext())!!
+        //database = AppDatabase.getInstance(requireContext())!!
+        firebaseDatabase = FirebaseDatabase.getInstance()
+        firebaseDatabase.getReference().child("users").orderByChild("ifArtist").equalTo(true).addListenerForSingleValueEvent(object: ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                var userList: ArrayList<DataSnapshot> = ArrayList()
+                for(item in snapshot.children){
+                    userList.add(item)
+                }
+                val adapter = ArtistsProfileRV(requireContext())
+                adapter.addProfile(userList)
+                binding.artistsRv.adapter = adapter
+                binding.artistsRv.layoutManager = LinearLayoutManager(context, GridLayoutManager.VERTICAL, false)
 
-        val adapter = ArtistsProfileRV(database.profileDao().getAllProfile() as ArrayList<Profile>,
-            requireContext()
-        )
-        binding.artistsRv.adapter = adapter
-        binding.artistsRv.layoutManager = LinearLayoutManager(context, GridLayoutManager.VERTICAL, false)
-
-        adapter.itemClickListener(object: ArtistsProfileRV.MyItemClickOnListener{
-            override fun profileArtworkClick(profile: Profile, database: AppDatabase) {
-                profileArtwork(profile, database)
+                adapter.itemClickListener(object: ArtistsProfileRV.MyItemClickOnListener{
+                    override fun profileArtworkClick(profile: UserModel) {
+                        profileArtwork(profile)
+                    }
+                })
             }
+
+            override fun onCancelled(error: DatabaseError) {}
         })
         return binding.root
     }
 
+
+
     //화가 대표작 클릭 시
-    private fun profileArtwork(profile: Profile, database: AppDatabase){
+    private fun profileArtwork(profile: UserModel){
         (context as MainActivity).supportFragmentManager.beginTransaction()
             .replace(R.id.main_frame, GalleryInfoFragment().apply {
                 arguments = Bundle().apply {
-                    val bestArtwork = profile.bestArtwork
-                    val gallery = database.galleryDao().getBestArtwork(bestArtwork.toString())
                     val gson = Gson()
                     val galleryJson = gson.toJson(gallery)
                     putString("gallery", galleryJson)
