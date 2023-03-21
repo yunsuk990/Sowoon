ba0c15ab9f6c35471e4a8a74c7a8dd4df9565974
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-03-21 23:47:00 +0900

    Update ArtistsFragment.kt

--- app/src/main/java/com/example/sowoon/ArtistsFragment.kt
+++ app/src/main/java/com/example/sowoon/ArtistsFragment.kt
@@ -5,22 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.room.Database
-import com.example.sowoon.data.entity.Gallery
-import com.example.sowoon.data.entity.Profile
-import com.example.sowoon.data.entity.UserModel
-import com.example.sowoon.data.entity.reference
 import com.example.sowoon.database.AppDatabase
 import com.example.sowoon.databinding.FragmentArtistsBinding
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
-import com.google.firebase.ktx.Firebase
-import com.google.gson.Gson
 
 class ArtistsFragment : Fragment() {
 
