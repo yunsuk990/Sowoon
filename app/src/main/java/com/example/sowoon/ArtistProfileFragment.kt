57e97acb1bdb4209cd2044e8ec4d86fd29a0f5dc
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-03-21 23:46:05 +0900

    Update ArtistProfileFragment.kt

--- app/src/main/java/com/example/sowoon/ArtistProfileFragment.kt
+++ app/src/main/java/com/example/sowoon/ArtistProfileFragment.kt
@@ -1,29 +1,24 @@
 package com.example.sowoon
 
-import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
-import com.example.sowoon.data.entity.Gallery
-import com.example.sowoon.data.entity.Profile
-import com.example.sowoon.data.entity.User
-import com.example.sowoon.data.entity.UserModel
-import com.example.sowoon.database.AppDatabase
+import com.example.sowoon.data.entity.*
 import com.example.sowoon.databinding.FragmentArtistProfileBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.gson.Gson
 
 class ArtistProfileFragment : Fragment() {
 
     lateinit var binding: FragmentArtistProfileBinding
     private var gson = Gson()
-    lateinit var database: AppDatabase
-    var uri: Uri? = null
+    lateinit var firebaseDatabase: FirebaseDatabase
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,11 +26,12 @@ class ArtistProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentArtistProfileBinding.inflate(inflater, container, false)
-        database = AppDatabase.getInstance(requireContext())!!
+        firebaseDatabase = FirebaseDatabase.getInstance()
+
         val profileJson = arguments?.getString("profile")
-        val profile = gson.fromJson(profileJson, UserModel::class.java)
+        var profile = gson.fromJson(profileJson, UserModel::class.java)
         setProfile(profile)
-        //setGridView()
+        setGridView(profile)
         return binding.root
     }
 
@@ -47,32 +43,39 @@ class ArtistProfileFragment : Fragment() {
         Glide.with(requireContext()).load(profile.profileImg).into(binding.artistsProfileIv)
     }
 
-//    private fun setGridView() {
-//        var datas = database.galleryDao().getUserGallery(getJwt()) as ArrayList<Gallery>
-//        var gridView = binding.artistsProfileGv
-//        var adapter = ArtistGalleryGVAdapter(datas as ArrayList<Gallery>, requireContext())
-//        adapter.itemClickListener(object: ArtistGalleryGVAdapter.MyItemClickListener{
-//            override fun artworkClick(gallery: Gallery) {
-//                ArtworkClick(gallery)
-//            }
-//        })
-//        gridView.adapter = adapter
-//
-//    }
+    private fun setGridView(profile: UserModel) {
+        firebaseDatabase.getReference().child("images").orderByChild("artist").equalTo(profile.name).addValueEventListener(object:
+            ValueEventListener {
+            var galleryList = ArrayList<GalleryModel>()
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for( item in snapshot.children){
+                    if(item.key != profile.profileModel?.key){
+                        var galleryModel = item.getValue(GalleryModel::class.java)
+                        galleryList.add(galleryModel!!)
+                    }
+                }
+                var gridView = binding.artistsProfileGv
+                var adapter = ArtistGalleryGVAdapter(galleryList, requireContext())
+                adapter.itemClickListener(object: ArtistGalleryGVAdapter.MyItemClickListener{
+                    override fun artworkClick(gallery: GalleryModel) {
+                        ArtworkClick(gallery)
+                    }
+                })
+                gridView.adapter = adapter
+            }
 
-    private fun getJwt(): Int {
-        val spf = activity?.getSharedPreferences("auth", AppCompatActivity.MODE_PRIVATE)
-        var jwt = spf?.getInt("jwt", 0)
-        return jwt!!
+            override fun onCancelled(error: DatabaseError) {}
+        })
     }
 
-    private fun ArtworkClick(gallery: Gallery) {
+    private fun ArtworkClick(gallery: GalleryModel) {
         (context as MainActivity).supportFragmentManager.beginTransaction()
             .replace(R.id.main_frame, GalleryInfoFragment().apply {
                 arguments = Bundle().apply {
-                    val gson = Gson()
-                    val galleryJson = gson.toJson(gallery)
-                    putString("gallery", galleryJson)
+                    var galleryJson = gson.toJson(gallery)
+                    var bundle = Bundle()
+                    bundle.putString("gallery", galleryJson)
+                    arguments = bundle
                 }
             })
             .commitNowAllowingStateLoss()
