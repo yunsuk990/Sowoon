6065bcc572fe985bb300d11f9893c053dfc47de6
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-03-21 23:46:11 +0900

    Update ArtistsProfileRV.kt

--- app/src/main/java/com/example/sowoon/ArtistsProfileRV.kt
+++ app/src/main/java/com/example/sowoon/ArtistsProfileRV.kt
@@ -1,27 +1,19 @@
 package com.example.sowoon
 
 import android.content.Context
-import android.net.Uri
 import android.os.Bundle
-import android.provider.ContactsContract.Data
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ImageView
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.net.toUri
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.sowoon.data.entity.*
-import com.example.sowoon.database.AppDatabase
 import com.example.sowoon.databinding.ItemArtistsprofileBinding
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
-import com.google.firebase.storage.FirebaseStorage
-import com.google.firebase.storage.StorageReference
-import com.google.firebase.storage.ktx.storage
 import com.google.gson.Gson
 
 class ArtistsProfileRV(var context: Context): RecyclerView.Adapter<ArtistsProfileRV.ViewHolder>() {
@@ -29,45 +21,54 @@ class ArtistsProfileRV(var context: Context): RecyclerView.Adapter<ArtistsProfil
     var profileList: ArrayList<DataSnapshot> = ArrayList()
     var firebaseDatabase: FirebaseDatabase = FirebaseDatabase.getInstance()
 
-
-    interface MyItemClickOnListener {
-        fun profileArtworkClick(profile: UserModel)
-    }
-
-    private lateinit var mItemClickListener: MyItemClickOnListener
-
-    fun itemClickListener(itemClick: MyItemClickOnListener){
-        mItemClickListener = itemClick
-    }
-
     inner class ViewHolder(val binding: ItemArtistsprofileBinding): RecyclerView.ViewHolder(binding.root){
         fun bind(profile: UserModel){
             var profileImg = profile.profileImg
             var profileModel = profile.profileModel
             var bestGallery = profileModel?.bestArtwork
+            var snapshot1: DataSnapshot? = null
 
             firebaseDatabase.reference.child("images").child(profileModel?.key!!).addValueEventListener(object: ValueEventListener{
                 override fun onDataChange(snapshot: DataSnapshot) {
+                    snapshot1 = snapshot
                     var galleryModel: GalleryModel? = snapshot.getValue(GalleryModel::class.java)
                     binding.profileArtistArtworkTv.text = galleryModel?.title
                     binding.profileArtistArtworkInfoTv.text = galleryModel?.info
-                    notifyDataSetChanged()
+
+                    binding.profileIv.scaleType = (ImageView.ScaleType.FIT_XY)
+                    Glide.with(context).load(profileImg).into(binding.profileIv)
+                    Glide.with(context).load(bestGallery).into(binding.profileArtistArtworkIv)
+                    binding.profileArtistAgeTv.text = profileModel?.school
+                    binding.profileArtistNameTv.text = profile.name
+
+//                    notifyDataSetChanged()
                 }
                 override fun onCancelled(error: DatabaseError) {}
             })
-            binding.profileIv.scaleType = (ImageView.ScaleType.FIT_XY)
-            Glide.with(context).load(profileImg).into(binding.profileIv)
-            Glide.with(context).load(bestGallery).into(binding.profileArtistArtworkIv)
-            binding.profileArtistAgeTv.text = profileModel?.school
-            binding.profileArtistNameTv.text = profile.name
 
-            binding.artistsProfile.setOnClickListener{
+            binding.profileIv.setOnClickListener{
                 (context as MainActivity).supportFragmentManager.beginTransaction()
                     .replace(R.id.main_frame, ArtistProfileFragment().apply {
                         arguments = Bundle().apply {
                             val gson = Gson()
                             val profileJson = gson.toJson(profile)
                             putString("profile", profileJson)
+                        }
+                    })
+                    .commitNowAllowingStateLoss()
+            }
+
+            binding.profileArtistArtworkIv.setOnClickListener {
+                Log.d("clicked", "true")
+                Log.d("clicked", snapshot1.toString())
+                var ref = reference(snapshot1!!)
+                (context as MainActivity).supportFragmentManager.beginTransaction()
+                    .replace(R.id.main_frame, GalleryInfoFragment().apply {
+                        arguments = Bundle().apply {
+//                                    val gson = Gson()
+//                                    val galleryJson = gson.toJson(gallery)
+//                                    putString("gallery", galleryJson)
+                            putSerializable("gallery" ,ref)
                         }
                     })
                     .commitNowAllowingStateLoss()
@@ -84,9 +85,6 @@ class ArtistsProfileRV(var context: Context): RecyclerView.Adapter<ArtistsProfil
         var profile: UserModel? = profileList[position].getValue(UserModel::class.java)
         if (profile != null) {
             holder.bind(profile)
-            holder.binding.profileArtistArtworkIv.setOnClickListener {
-                mItemClickListener.profileArtworkClick(profile)
-            }
         }
     }
 
