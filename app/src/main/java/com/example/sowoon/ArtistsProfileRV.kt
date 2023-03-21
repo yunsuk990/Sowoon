9ffaba832c8a4da6756229effc66a8c279cf2d1f
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-03-22 00:54:45 +0900

    Update ArtistsProfileRV.kt

--- app/src/main/java/com/example/sowoon/ArtistsProfileRV.kt
+++ app/src/main/java/com/example/sowoon/ArtistsProfileRV.kt
@@ -18,7 +18,7 @@ import com.google.gson.Gson
 
 class ArtistsProfileRV(var context: Context): RecyclerView.Adapter<ArtistsProfileRV.ViewHolder>() {
 
-    var profileList: ArrayList<DataSnapshot> = ArrayList()
+    var profileList: ArrayList<UserModel> = ArrayList()
     var firebaseDatabase: FirebaseDatabase = FirebaseDatabase.getInstance()
 
     inner class ViewHolder(val binding: ItemArtistsprofileBinding): RecyclerView.ViewHolder(binding.root){
@@ -26,11 +26,8 @@ class ArtistsProfileRV(var context: Context): RecyclerView.Adapter<ArtistsProfil
             var profileImg = profile.profileImg
             var profileModel = profile.profileModel
             var bestGallery = profileModel?.bestArtwork
-            var snapshot1: DataSnapshot? = null
-
             firebaseDatabase.reference.child("images").child(profileModel?.key!!).addValueEventListener(object: ValueEventListener{
                 override fun onDataChange(snapshot: DataSnapshot) {
-                    snapshot1 = snapshot
                     var galleryModel: GalleryModel? = snapshot.getValue(GalleryModel::class.java)
                     binding.profileArtistArtworkTv.text = galleryModel?.title
                     binding.profileArtistArtworkInfoTv.text = galleryModel?.info
@@ -40,39 +37,31 @@ class ArtistsProfileRV(var context: Context): RecyclerView.Adapter<ArtistsProfil
                     Glide.with(context).load(bestGallery).into(binding.profileArtistArtworkIv)
                     binding.profileArtistAgeTv.text = profileModel?.school
                     binding.profileArtistNameTv.text = profile.name
+                    binding.profileIv.setOnClickListener{
+                        (context as MainActivity).supportFragmentManager.beginTransaction()
+                            .replace(R.id.main_frame, ArtistProfileFragment().apply {
+                                arguments = Bundle().apply {
+                                    val gson = Gson()
+                                    val profileJson = gson.toJson(profile)
+                                    putString("profile", profileJson)
+                                }
+                            })
+                            .commitNowAllowingStateLoss()
+                    }
 
-//                    notifyDataSetChanged()
+                    binding.profileArtistArtworkIv.setOnClickListener {
+                        (context as MainActivity).supportFragmentManager.beginTransaction()
+                            .replace(R.id.main_frame, GalleryInfoFragment().apply {
+                                var galleryJson = gson.toJson(galleryModel)
+                                var bundle = Bundle()
+                                bundle.putString("gallery", galleryJson)
+                                arguments = bundle
+                            })
+                            .commitNowAllowingStateLoss()
+                    }
                 }
                 override fun onCancelled(error: DatabaseError) {}
             })
-
-            binding.profileIv.setOnClickListener{
-                (context as MainActivity).supportFragmentManager.beginTransaction()
-                    .replace(R.id.main_frame, ArtistProfileFragment().apply {
-                        arguments = Bundle().apply {
-                            val gson = Gson()
-                            val profileJson = gson.toJson(profile)
-                            putString("profile", profileJson)
-                        }
-                    })
-                    .commitNowAllowingStateLoss()
-            }
-
-            binding.profileArtistArtworkIv.setOnClickListener {
-                Log.d("clicked", "true")
-                Log.d("clicked", snapshot1.toString())
-                var ref = reference(snapshot1!!)
-                (context as MainActivity).supportFragmentManager.beginTransaction()
-                    .replace(R.id.main_frame, GalleryInfoFragment().apply {
-                        arguments = Bundle().apply {
-//                                    val gson = Gson()
-//                                    val galleryJson = gson.toJson(gallery)
-//                                    putString("gallery", galleryJson)
-                            putSerializable("gallery" ,ref)
-                        }
-                    })
-                    .commitNowAllowingStateLoss()
-            }
         }
     }
 
@@ -82,15 +71,16 @@ class ArtistsProfileRV(var context: Context): RecyclerView.Adapter<ArtistsProfil
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        var profile: UserModel? = profileList[position].getValue(UserModel::class.java)
+        var profile: UserModel? = profileList[position]
         if (profile != null) {
             holder.bind(profile)
         }
+
     }
 
     override fun getItemCount(): Int = profileList.size
 
-    fun addProfile(userList: ArrayList<DataSnapshot>){
+    fun addProfile(userList: ArrayList<UserModel>){
         profileList.clear()
         profileList.addAll(userList)
         notifyDataSetChanged()
