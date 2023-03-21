4ee7a44a704098c932ec3475f1dc7ecf1e87be12
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-03-21 23:46:16 +0900

    Update GalleryModel.kt

--- app/src/main/java/com/example/sowoon/data/entity/GalleryModel.kt
+++ app/src/main/java/com/example/sowoon/data/entity/GalleryModel.kt
@@ -3,6 +3,7 @@ package com.example.sowoon.data.entity
 import androidx.room.PrimaryKey
 
 class GalleryModel {
+    var galleryKey: String? = null
     var uid: String? = null
     var favorites: ArrayList<String>? = ArrayList()
     var title: String? = null
