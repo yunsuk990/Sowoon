2f080fd8e58f6dbd159a38940d594fc570bc53f5
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-03-21 23:46:57 +0900

    Delete reference.kt

--- app/src/main/java/com/example/sowoon/data/entity/reference.kt
+++
@@ -1,8 +0,0 @@
-package com.example.sowoon.data.entity
-
-import com.google.firebase.database.DataSnapshot
-import java.io.Serializable
-
-class reference(gallery: DataSnapshot) : Serializable {
-    var gallery: DataSnapshot = gallery
-}
