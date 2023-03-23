923ef8c36f8dfd02ac70816ab3a598de4bb011cd
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-03-23 23:59:07 +0900

    Update RegistArtistActivity.kt

--- app/src/main/java/com/example/sowoon/RegistArtistActivity.kt
+++ app/src/main/java/com/example/sowoon/RegistArtistActivity.kt
@@ -12,9 +12,7 @@ import android.widget.ImageView
 import android.widget.Toast
 import androidx.core.net.toUri
 import com.bumptech.glide.Glide
-import com.example.sowoon.data.entity.Profile
-import com.example.sowoon.data.entity.ProfileModel
-import com.example.sowoon.data.entity.User
+import com.example.sowoon.data.entity.*
 import com.example.sowoon.database.AppDatabase
 import com.example.sowoon.databinding.ActivityRegistArtistBinding
 import com.google.android.gms.tasks.OnCompleteListener
@@ -23,7 +21,10 @@ import com.google.android.gms.tasks.Task
 import com.google.android.gms.tasks.Tasks.await
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
@@ -53,6 +54,7 @@ class RegistArtistActivity : AppCompatActivity() {
         database = AppDatabase.getInstance(this)!!
         firebaseAuth = FirebaseAuth.getInstance()
         firebaseDatabase = FirebaseDatabase.getInstance()
+
 
         //binding.myInfoName.text = getName()
 
@@ -86,23 +88,48 @@ class RegistArtistActivity : AppCompatActivity() {
     }
 
     private fun uploadImage(imageUri: Uri?){
+        var userModel: UserModel? = null
+        firebaseDatabase.reference.child("users").child(currentUser!!.uid).addListenerForSingleValueEvent(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                userModel = snapshot.getValue(UserModel::class.java)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
         var key = imageUri?.lastPathSegment.toString()
+        var galleryModel = GalleryModel()
         var mountainImageRef: StorageReference? = storage?.reference?.child("images")?.child(key)
-        mountainImageRef?.putFile(imageUri!!)?.addOnSuccessListener {
+        mountainImageRef?.putFile(imageUri!!)?.addOnCompleteListener{
             mountainImageRef.downloadUrl.addOnSuccessListener { url ->
                 var path = url.toString()
                 var profileModel = ProfileModel()
+
                 profileModel.bestArtwork = path
                 profileModel.awards = binding.myInfoAwardsInput.text.toString()
                 profileModel.school = binding.myInfoSchoolInput.text.toString()
                 profileModel.key = key
+
+                galleryModel.galleryKey = key
+                galleryModel.uid = currentUser?.uid
+                galleryModel.artist = userModel?.name
+                galleryModel.imagePath = path
+
+//                var title: String? = null
+//                var info: String? = null
+//                var likeUid: ArrayList<String> = ArrayList()
                 var map: MutableMap<String, Any> = HashMap()
                 map.put("ifArtist", true)
                 map.put("profileModel", profileModel)
-                firebaseDatabase.getReference().child("users").child(currentUser!!.uid).updateChildren(map)
-                //firebaseDatabase.getReference().child("profile").child(currentUser!!.uid).setValue(profileModel)
+                map.put("profieImgKey", key)
+                firebaseDatabase.reference.child("users").child(currentUser!!.uid).updateChildren(map)
+                firebaseDatabase.reference.child("images").child(key).setValue(galleryModel)
             }
         }
+
+
     }
 
     private fun openBestGallery() {
