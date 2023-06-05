46d0f7696ffe3920d37e6adb18c13c674854f639
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-06-05 16:21:59 +0900

    Update ArtistsFragment.kt

--- app/src/main/java/com/example/sowoon/view/ArtistsFragment.kt
+++ app/src/main/java/com/example/sowoon/view/ArtistsFragment.kt
@@ -1,15 +1,18 @@
 package com.example.sowoon.view
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.sowoon.data.entity.UserModel
 import com.example.sowoon.database.AppDatabase
 import com.example.sowoon.databinding.FragmentArtistsBinding
+import com.example.sowoon.viewmodel.ArtistViewModel
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -18,8 +21,7 @@ import com.google.firebase.database.ValueEventListener
 class ArtistsFragment : Fragment() {
 
     lateinit var binding: FragmentArtistsBinding
-    lateinit var database: AppDatabase
-    lateinit var firebaseDatabase: FirebaseDatabase
+    lateinit var artistViewModel: ArtistViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,25 +29,15 @@ class ArtistsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentArtistsBinding.inflate(inflater, container, false)
-        //database = AppDatabase.getInstance(requireContext())!!
-        firebaseDatabase = FirebaseDatabase.getInstance()
-        val adapter = ArtistsProfileRV(requireContext())
-        binding.artistsRv.adapter = adapter
-        binding.artistsRv.layoutManager = LinearLayoutManager(context, GridLayoutManager.VERTICAL, false)
-        firebaseDatabase.getReference().child("users").orderByChild("ifArtist").equalTo(true).addListenerForSingleValueEvent(object: ValueEventListener{
-            override fun onDataChange(snapshot: DataSnapshot) {
-                var userList: ArrayList<UserModel> = ArrayList()
-                for(item in snapshot.children){
-                    var profileModel = item.getValue(UserModel::class.java)
-                    if (profileModel != null) {
-                        userList.add(profileModel)
-                    }
-                }
-                adapter.addProfile(userList)
-            }
-
-            override fun onCancelled(error: DatabaseError) {}
+        artistViewModel = ArtistViewModel()
+        artistViewModel.artistsProfileLiveData.observe(viewLifecycleOwner, Observer {
+            val adapter = ArtistsProfileRV(requireContext())
+            binding.artistsRv.adapter = adapter
+            binding.artistsRv.layoutManager = LinearLayoutManager(context, GridLayoutManager.VERTICAL, false)
+            adapter.addProfile(it)
+            binding.artistsProgressbar.visibility = View.INVISIBLE
         })
+        artistViewModel.getArtistProfile()
         return binding.root
     }
 }
