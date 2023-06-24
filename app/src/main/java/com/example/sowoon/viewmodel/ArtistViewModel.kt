df4e87bd8b9cdad58845b19d92c2b8b3b959bfab
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-06-24 23:12:30 +0900

    Update ArtistViewModel.kt

--- app/src/main/java/com/example/sowoon/viewmodel/ArtistViewModel.kt
+++ app/src/main/java/com/example/sowoon/viewmodel/ArtistViewModel.kt
@@ -8,13 +8,20 @@ import com.example.sowoon.repository.FirebaseRepository
 class ArtistViewModel: ViewModel() {
 
     var artistsProfileLiveData: MutableLiveData<ArrayList<UserModel>>
+    var userProfileLiveData: MutableLiveData<UserModel>
     val firebaseRepository: FirebaseRepository = FirebaseRepository()
+
 
     init {
         artistsProfileLiveData = firebaseRepository.artistsProfileLiveData
+        userProfileLiveData = firebaseRepository.userProfileLiveData
     }
 
     fun getArtistProfile() = firebaseRepository.getArtistProfile()
+
+    fun getUserProfile(uid: String) = firebaseRepository.getUserProfile(uid)
+
+
 
 
 
