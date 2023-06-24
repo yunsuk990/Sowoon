4f0231f64bfc99d305d696e4a44f0d57c21874d0
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-06-24 23:12:37 +0900

    Update RegistArtistActivity.kt

--- app/src/main/java/com/example/sowoon/view/RegistArtistActivity.kt
+++ app/src/main/java/com/example/sowoon/view/RegistArtistActivity.kt
@@ -7,9 +7,11 @@ import android.os.Bundle
 import android.provider.MediaStore
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.lifecycle.Observer
 import com.example.sowoon.data.entity.*
 import com.example.sowoon.database.AppDatabase
 import com.example.sowoon.databinding.ActivityRegistArtistBinding
+import com.example.sowoon.viewmodel.ArtistViewModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DataSnapshot
@@ -25,14 +27,15 @@ import com.google.gson.Gson
 class RegistArtistActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityRegistArtistBinding
-    lateinit var database: AppDatabase
     lateinit var storage: FirebaseStorage
     lateinit var firebaseAuth: FirebaseAuth
     lateinit var firebaseDatabase: FirebaseDatabase
     lateinit var gson: Gson
+    lateinit var artistViewModel: ArtistViewModel
     val Best_GALLERY = 20
     var bestImageURL: Uri? = null
     var currentUser: FirebaseUser? = null
+    var userModel: UserModel? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,10 +44,19 @@ class RegistArtistActivity : AppCompatActivity() {
         storage = Firebase.storage
         firebaseAuth = FirebaseAuth.getInstance()
         firebaseDatabase = FirebaseDatabase.getInstance()
-//이름 설정        binding.myInfoName.text =
 
+
+        artistViewModel = ArtistViewModel()
+        artistViewModel.userProfileLiveData.observe(this, Observer{
+            binding.myInfoName.text = it.name
+            userModel = it
+        })
         binding.registBtn.setOnClickListener {
-            registInfo()
+            if(bestImageURL == null){
+                Toast.makeText(this, "대표작을 등록해주세요.", Toast.LENGTH_SHORT).show()
+            }else{
+                registInfo()
+            }
         }
         binding.myInfoBestArtworkIv.setOnClickListener{
             openBestGallery()
@@ -54,43 +66,23 @@ class RegistArtistActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
         currentUser = firebaseAuth.currentUser
+        artistViewModel.getUserProfile(currentUser?.uid.toString())
     }
 
     private fun registInfo(){
         uploadImage(bestImageURL)
-        //Firebase의 비동기식 처리로 인한 처리 불가
-//        var firebaseprofilepath: String? = uploadImage(profileURL)
-//        var firebasebestImagepath: String? = uploadImage(bestImageURL)
-//            var map: MutableMap<String, Any> = HashMap()
-//            map.put("ifArtist", true)
-//            if (firebaseprofilepath != null) {
-//                map.put("profileImg", firebaseprofilepath!!)
-//            }
-//            firebaseDatabase.getReference().child("users").child(currentUser!!.uid).updateChildren(map)
-
         Toast.makeText(this, "등록되었습니다." ,Toast.LENGTH_SHORT).show()
         finish()
     }
 
     private fun uploadImage(imageUri: Uri?){
-        var userModel: UserModel? = null
-        firebaseDatabase.reference.child("users").child(currentUser!!.uid).addListenerForSingleValueEvent(object : ValueEventListener{
-            override fun onDataChange(snapshot: DataSnapshot) {
-                userModel = snapshot.getValue(UserModel::class.java)
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-
-            }
-
-        })
         var key = imageUri?.lastPathSegment.toString()
-        var galleryModel = GalleryModel()
         var mountainImageRef: StorageReference? = storage?.reference?.child("images")?.child(key)
         mountainImageRef?.putFile(imageUri!!)?.addOnCompleteListener{
             mountainImageRef.downloadUrl.addOnSuccessListener { url ->
                 var path = url.toString()
                 var profileModel = ProfileModel()
+                var galleryModel = GalleryModel()
 
                 profileModel.bestArtwork = path
                 profileModel.awards = binding.myInfoAwardsInput.text.toString()
@@ -101,14 +93,13 @@ class RegistArtistActivity : AppCompatActivity() {
                 galleryModel.uid = currentUser?.uid
                 galleryModel.artist = userModel?.name
                 galleryModel.imagePath = path
+                galleryModel.title = binding.myInfoTitleInput.text.toString()
+                galleryModel.info = binding.myInfoInfoInput.text.toString()
 
-//                var title: String? = null
-//                var info: String? = null
-//                var likeUid: ArrayList<String> = ArrayList()
                 var map: MutableMap<String, Any> = HashMap()
                 map.put("ifArtist", true)
                 map.put("profileModel", profileModel)
-                map.put("profieImgKey", key)
+                map.put("profileImgKey", key)
                 firebaseDatabase.reference.child("users").child(currentUser!!.uid).updateChildren(map)
                 firebaseDatabase.reference.child("images").child(key).setValue(galleryModel)
             }
