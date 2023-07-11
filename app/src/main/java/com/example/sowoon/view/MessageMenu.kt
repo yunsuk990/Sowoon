279caaca53b41f8ba748b6afb8deb04f194e1b12
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-07-11 23:57:35 +0900

    Create MessageMenu.kt

---
+++ app/src/main/java/com/example/sowoon/view/MessageMenu.kt
@@ -0,0 +1,31 @@
+package com.example.sowoon.view
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.sowoon.databinding.ActivityMessageMenuBinding
+import com.example.sowoon.view.adapter.MessageMenuAdapter
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.FirebaseDatabase
+
+class MessageMenu : AppCompatActivity() {
+
+    lateinit var binding: ActivityMessageMenuBinding
+    lateinit var firebaseAuth: FirebaseAuth
+    lateinit var firebaseDatabase: FirebaseDatabase
+    var currentUser: FirebaseUser? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMessageMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        firebaseAuth = FirebaseAuth.getInstance()
+        firebaseDatabase = FirebaseDatabase.getInstance()
+        currentUser = firebaseAuth.currentUser
+
+        var adapter = MessageMenuAdapter(this)
+        binding.messagemenuRv.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        binding.messagemenuRv.adapter = adapter
+    }
+
+}
